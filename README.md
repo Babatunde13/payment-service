000544d8b7c405f4a1819d46133d92a4a9f20035
Babatunde Koiki <koikibabatunde14@gmail.com>
2024-05-23 20:00:46 +0100

    (feat): setup codebase

---
+++ README.md
@@ -0,0 +1,8 @@
+## Payment Gateway Integrations
+
+This repository contains code different services that helps in integrating payment gateways with your application.
+
+### Installation
+```bash
+npm install payment-gateway-integrations
+```
---
+++ lib/base.service.ts
@@ -0,0 +1,43 @@
+export class BaseService {
+    public secretKey: string
+    public baseUrl: string
+
+    constructor(secretKey: string, baseUrl: string) {
+        if (!secretKey) {
+            throw new Error('Secret key is required')
+        }
+
+        if (!baseUrl) {
+            throw new Error('Base URL is required')
+        }
+
+        this.secretKey = secretKey
+        this.baseUrl = baseUrl
+    }
+
+    buildUrlWithQueryParam = (url: string, query: { [key: string]: Any }) => {
+        let urlWithQueryParam = url
+        Object.keys(query).forEach(key => {
+            if (urlWithQueryParam.includes('?')) {
+                urlWithQueryParam += `&${key}=${query[key]}`
+            } else {
+                urlWithQueryParam += `?${key}=${query[key]}`
+            }
+        })
+
+        return urlWithQueryParam
+    }
+}
+
+export type Any = any
+
+type Response<T> =
+    {
+        data: T
+        error?: undefined
+    } |{
+        data?: undefined
+        error: Any
+    }
+
+export type BaseResponse<T> = Promise<Response<T>>
